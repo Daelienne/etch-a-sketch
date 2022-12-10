25ac063134d3b44caa3fd7bb7e0e2f8ca434550a
Daelienne <daelienne@gmail.com>
2022-12-10 14:24:16 +0100

    Remove scale property from container and improve design

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-const container = document.querySelector(".container");
+// const container = document.querySelector(".container");
+// const slider = document.querySelector(".slider");
 
-function createDivs (rows, columns) {
-    container.style.setProperty("--grid-rows", rows);
-    container.style.setProperty("--grid-columns", columns);
+// // const slider = document.getElementById("myRange");
+// // const output = document.getElementById("demo");
 
-    for (let i = 0; i < (rows * columns); i++) {
-        let cell = document.createElement("div");
-        //cell.innerText = (i + 1);
-        container.appendChild(cell).className = "grid-item";
+// // output.innerText = slider.value;
 
-        cell.addEventListener("mouseover",
-        e => e.target.classList.add("black-color")
-        );       
-    }; 
-};
+// // slider.oninput = function() {
+// //     output.innerText = this.value;
+// // }
 
-createDivs(16, 16);
+// function createDivs (rows, columns) {
+//     container.style.setProperty("--grid-rows", rows);
+//     container.style.setProperty("--grid-columns", columns);
+
+//     for (let i = 0; i < (rows * columns); i++) {
+//         let cell = document.createElement("div");
+//         //cell.innerText = (i + 1);
+//         container.appendChild(cell).className = "grid-item";
+
+//         cell.addEventListener("mouseover",
+//         e => e.target.classList.add("black-color")
+//         );       
+//     }; 
+// };
+
+// createDivs(16, 16);
+
+
 
