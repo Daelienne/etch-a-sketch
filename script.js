c3f5f0b0a27fad86998e8eb141a83ed895ce39e0
Daelienne <daelienne@gmail.com>
2022-12-11 13:46:03 +0100

    Color divs on hover

--- script.js
+++ script.js
@@ -1,31 +1,59 @@
-// const container = document.querySelector(".container");
-// const slider = document.querySelector(".slider");
+ const container = document.querySelector(".container");
+ const sizeSelectorButton = document.querySelector(".select-size");
+ const blackButton = document.querySelector(".black");
+ const rainbowButton = document.querySelector(".rainbow");
+ const eraserButton = document.querySelector(".eraser");
+ const clearButton = document.querySelector(".clear");
 
-// // const slider = document.getElementById("myRange");
-// // const output = document.getElementById("demo");
+//  let color = "black";
 
-// // output.innerText = slider.value;
+ sizeSelectorButton.addEventListener("click",() => {
+    let size = getUserInputSize();
+    createDivs(size);
+ });
 
-// // slider.oninput = function() {
-// //     output.innerText = this.value;
-// // }
+//  blackButton.addEventListener("click", () => {
+    
+//  });
 
-// function createDivs (rows, columns) {
-//     container.style.setProperty("--grid-rows", rows);
-//     container.style.setProperty("--grid-columns", columns);
+ function createDivs(size) {
 
-//     for (let i = 0; i < (rows * columns); i++) {
-//         let cell = document.createElement("div");
-//         //cell.innerText = (i + 1);
-//         container.appendChild(cell).className = "grid-item";
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-//         cell.addEventListener("mouseover",
-//         e => e.target.classList.add("black-color")
-//         );       
-//     }; 
-// };
+    for (let i = 0; i < (size * size); i++) {
+        let cell = document.createElement("div");
+        container.appendChild(cell).className = "cell";
 
-// createDivs(16, 16);
+        cell.addEventListener("mouseover",
+         e => e.target.classList.add("black-color")
+        );
+    }
+ } 
+ createDivs(16);
+ 
+ function getUserInputSize() {
+
+    let userInput = prompt("Enter a size value between 1 and 100.");
+    let message = document.querySelector(".message");
+
+    if ( userInput > 0 && userInput <= 100) {
+        message.textContent = "Size board changed."
+        return userInput;
+    } else if (userInput <= 0 || userInput > 100) {
+        message.textContent = "Please, enter a value between 1 and 100.";
+    } else {
+        message.textContent = "Please, enter a number."
+    }
+ }
+
+//  function colorDiv() {
+//     if(color == "rainbow") {
+//         this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+//     } else {
+//         this.style.backgroundColor = "black";
+//     }
+//  }
 
 
-
+ 
